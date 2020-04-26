09b8d291b08a578cf133cc52334e73f73f77e555
Leostorm <etiennedoucetleger@cmail.carleton.ca>
2020-04-25 22:37:49 -0400

    added text prefab

---
+++ Assets/Text_holder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text_holder : MonoBehaviour
+{
+    public string TEXT_INFO;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
